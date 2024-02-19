ad38e99c6c0a3a117c87b154f768df3e79afae92
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-02-19 20:34:23 +0100

    tailwind theme extended colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundColor: '#020916',
+        accentColor: '#CEFF7E',
+        textColors: '#fff',
+        secondTextColor: '#9CA3AF',
+      },
+      fontFamily: {
+        mainFontFamily: ['DM Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }

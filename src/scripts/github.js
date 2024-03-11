e1293c40e843e2f45c5f49d211e7aad32ebc1ae7
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-03-11 21:04:27 +0100

    fixed github apy func

--- src/scripts/github.js
+++ src/scripts/github.js
@@ -1,3 +1,5 @@
+import { demoIcon } from '../img/demo-icon.svg'
+
 export const fetchRepositories = () => {
     const githubUser = "krzysiekkalinowski";
     const githubDirection = "desc";
@@ -9,7 +11,14 @@ export const fetchRepositories = () => {
         .then((response) => response.json())
         .then((response) => {
           for (let repository of response) {
-            const { name, html_url, description } = repository;
+            const { name, html_url, description, topics, homepage, stargazers_count } = repository;
+
+            let tags = '';
+
+            for (tag of topics) {
+                tags += `<span class="py-1 px-2 rounded-md bg-accentBackgroundColor">${tag}</span>`;
+            }
+
             const htmlString = `<article class="rounded-xl overflow-hidden flex flex-col bg-gradient-to-br from-white/10 to-white/5 shadow-articleShadow">
                 <div class="h-11 bg-gradient-to-br from-white/10 to-white/5 flex p-4 gap-2">
                     <span class="h-3 w-3 block rounded-imgRadius bg-backgroundColor/50"></span><span class="h-3 w-3 block rounded-imgRadius bg-backgroundColor/50"></span><span class="h-3 w-3 block rounded-imgRadius bg-backgroundColor/50"></span>
@@ -17,22 +26,23 @@ export const fetchRepositories = () => {
                 <div class="flex flex-col gap-y-4 p-5">
                     <div class="flex gap-x-4">
                     <h3 class="font-bold text-2xl">${name}</h3>
-                    <span class="inline-flex items-baseline gap-x-1 py-1 px-2 rounded-md bg-accentBackgroundColor"><img src="img/star.svg" /><p class="text-gray-400 counter--js">27</p></span>
+                    <span class="inline-flex items-baseline gap-x-1 py-1 px-2 rounded-md bg-accentBackgroundColor"><img src="img/star.svg" /><p class="text-gray-400 counter--js">${stargazers_count}</p></span>
                     </div>
                     <p class="text-xl leading-6 text-gray-400">
                     ${description}
                     </p>
-                    <div class="flex gap-x-1">
-                    <span class="py-1 px-2 rounded-md bg-accentBackgroundColor">HTML</span><span class="py-1 px-2 rounded-md bg-accentBackgroundColor">CSS</span><span class="py-1 px-2 rounded-md bg-accentBackgroundColor">Tailwind</span>
-                    <span class="py-1 px-2 rounded-md bg-accentBackgroundColor">JS</span>
+                    <div class="flex gap-x-1 flex-wrap">
+                    ${tags}
                     </div>
                 </div>
                 <div class="flex flex-col gap-y-4 p-5">
-                    <a href="#" class="py-4 px-5 bg-backgroundColor font-bold text-primaryColor flex gap-x-2 w-max rounded-xl"><img src="img/demo-icon.svg" />View demo</a>
-                    <a href="${html_url}" class="py-4 px-5 bg-backgroundColor font-bold text-primaryColor flex gap-x-2 w-max rounded-xl"><img src="img/github-icon.svg" />Source code</a>
+                    <a href="${homepage}" rel="nofollow noreferrer" target="_blank" class="py-4 px-5 bg-backgroundColor font-bold text-primaryColor flex gap-x-2 w-max rounded-xl"><img src="${demoIcon}" />View demo</a>
+                    <a href="${html_url}" rel="nofollow noreferrer" target="_blank" class="py-4 px-5 bg-backgroundColor font-bold text-primaryColor flex gap-x-2 w-max rounded-xl"><img src="img/github-icon.svg" />Source code</a>
                 </div>
                 </article>`;
-            projectsContainer.insertAdjacentHTML('afterbegin', htmlString);
+            if (homepage) {
+                projectsContainer.insertAdjacentHTML('afterbegin', htmlString);
+            }
         }
     })
     .catch(error => {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 import { hamburgerToggle } from "./hamburger.js";
 import { fetchRepositories } from "./github.js";
+import { demoIcon } from '../img/demo-icon.svg'
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchRepositories();
